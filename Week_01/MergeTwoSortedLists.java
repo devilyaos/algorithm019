360ace0d61e63b9c8afa11332da881eabdba04f5
姚舜 <devilyaos@gmail.com>
2020-11-07 15:52:01 +0800

    提交合并链表以及去除重复元素

--- Week_01/MergeTwoSortedLists.java
+++ Week_01/MergeTwoSortedLists.java
@@ -1,2 +1,71 @@
 public class MergeTwoSortedLists {
+    /**
+     * 递归
+     *
+     * @param l1 链表1
+     * @param l2 链表2
+     * @return 合并升序链表
+     */
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists1(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists1(l1, l2.next);
+            return l2;
+        }
+    }
+
+    /**
+     * 迭代
+     *
+     * @param l1 链表1
+     * @param l2 链表2
+     * @return 合并后的链表
+     */
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode a = l1;
+        ListNode b = l2;
+        ListNode result = new ListNode();
+        ListNode c = result;
+        while (a != null && b != null) {
+            if (a.val <= b.val) {
+                c.next = new ListNode(a.val);
+                a = a.next;
+            } else {
+                c.next = new ListNode(b.val);
+                b = b.next;
+            }
+            c = c.next;
+        }
+        if (a != null) {
+            c.next = a;
+        } else if (b != null) {
+            c.next = b;
+        }
+        return result.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
--- Week_01/RemoveDuplicatesFromSortedArray.java
+++ Week_01/RemoveDuplicatesFromSortedArray.java
@@ -1,2 +1,36 @@
 public class RemoveDuplicatesFromSortedArray {
+    /**
+     * 1、双指针遍历，一个指针遍历，一个指针指向下一个可以插入的位置.
+     *   因为数组给定条件为排序数组，所以与插入位置指针的上一位进行比较，相同则不插入
+     *   且题目仅要求前N位符合要求即可，LENGTH - N位的元素无需处理，所以直接从后往前复制即可
+     *
+     * @param nums 原始数组
+     * @return 不重复的元素数
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        // 排序数组，第一个元素一定不移动，所以直接从第二个元素开始
+        int a = 1;
+        int b = 1;
+        int len = nums.length;
+        while(a < len) {
+            if (nums[a] > nums[b - 1]) {
+                // 考虑到
+                nums[b++] = nums[a];
+            }
+            a++;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicatesFromSortedArray test = new RemoveDuplicatesFromSortedArray();
+        int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
+        int len = test.removeDuplicates(nums);
+        for (int i = 0; i < len; i++) {
+            System.out.println(nums[i]);
+        }
+    }
 }
