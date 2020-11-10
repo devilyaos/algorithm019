1b9f9eeb8245ffb87ee1eedd94e70d6615cfaf7e
姚舜 <devilyaos@gmail.com>
2020-11-11 00:41:19 +0800

    提交49题解

--- Week_02/Solution49.java
+++ Week_02/Solution49.java
@@ -8,11 +8,11 @@ public class Solution49 {
      * @param strs 待区分的数组
      * @return 分组
      */
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public List<List<String>> groupAnagrams1(String[] strs) {
         Map<String, List<String>> map = new HashMap<>(strs.length);
         String tmpStr;
         char[] tmpCharArr;
-        for (String s: strs) {
+        for (String s : strs) {
             tmpCharArr = s.toCharArray();
             Arrays.sort(tmpCharArr);
             tmpStr = new String(tmpCharArr);
@@ -20,6 +20,32 @@ public class Solution49 {
                 map.put(tmpStr, new ArrayList<>());
             }
             map.get(tmpStr).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    /**
+     * 字母异位词分组
+     * 2. 利用26个英文字母的特性减少sort开销
+     *
+     * @param strs 待区分的数组
+     * @return 分组
+     */
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        char[] letterArr = new char[26];
+        Map<String, List<String>> map = new HashMap<>(strs.length);
+        char[] tmpArr;
+        String tmpKey;
+        for (String s: strs) {
+            tmpArr = s.toCharArray();
+            for (char c: tmpArr) {
+                letterArr[c - 'a']++;
+            }
+            tmpKey = String.valueOf(letterArr);
+            if (!map.containsKey(tmpKey)) {
+                map.put(tmpKey, new LinkedList<>());
+            }
+            map.get(tmpKey).add(s);
         }
         return new ArrayList<>(map.values());
     }
