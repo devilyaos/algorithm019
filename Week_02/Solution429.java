968cdc00ea90f171c01029ee87c5ade8fec43c2e
姚舜 <devilyaos@gmail.com>
2020-11-14 01:21:40 +0800

    提交429题解

---
+++ Week_02/Solution429.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Solution429 {
+    /**
+     * N叉树的层序遍历
+     *
+     * @param root 根节点
+     * @return 遍历结果
+     */
+    public List<List<Integer>> levelOrder(Node root) {
+        if (root == null) {
+            return new LinkedList<>();
+        }
+        Queue<Node> queue = new LinkedList<>();
+        List<List<Integer>> result = new LinkedList<>();
+        queue.add(root);
+        result.add(Collections.singletonList(root.val));
+        while (!queue.isEmpty()) {
+            List<Integer> list = new LinkedList<>();
+            // 每次清空当前queue中内容，及该层全部内容
+            int queueSize = queue.size();
+            for (int i = 0; i < queueSize; i++) {
+                Node node = queue.poll();
+                list.add(node.val);
+                queue.addAll(node.children);
+            }
+            result.add(list);
+        }
+        return result;
+    }
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}
