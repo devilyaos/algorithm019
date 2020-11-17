c4a3a1696b09b63aab8e1bf76edb605f532ba30f
姚舜 <devilyaos@gmail.com>
2020-11-17 23:53:15 +0800

    提交226题解

---
+++ Week_03/Solution226.java
@@ -0,0 +1,26 @@
+public class Solution226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        // 此处需要考虑到反转后已经发生了变化，所以此处用临时变量先存储后赋值
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
